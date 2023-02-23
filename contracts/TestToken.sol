06e5410f918f2fb015a3a873775ef15885453ec5
OleanjiKingCode <holamilean56@gmail.com>
2023-02-23 22:03:23 +0100

    updating comments

--- contracts/TestToken.sol
+++ contracts/TestToken.sol
@@ -4,10 +4,17 @@ pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+/// @title Test token
+/// @author Oleanji
+/// @notice This contract is used as a test token to test the crowd funding contract
 contract TestToken is ERC20("TestToken", "Tst") {
+    
     //address of the deployer
     address ownerAddress;
-    
+
+    /// -----------------------------------------------------------------------
+    /// Constructor
+    /// -----------------------------------------------------------------------
     constructor(uint totalSupply) {
         uint amount = totalSupply * 10 ** 18;
         ownerAddress = msg.sender;
